b56d899729d6c37625b031a24c939a01e5db009b
getridofiot <getridofthem@protonmail.com>
2024-11-03 15:38:02 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://anura.pro/";//your website url
+  let target = "https://aluu.xyz/en/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
