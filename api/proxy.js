c5364a82cdfbde1c5cfb1d36d170b4c9623e79f5
getridofiot <getridofthem@protonmail.com>
2024-11-03 15:05:33 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://selenite.cc/";//your website url
+  let target = "https://browser.rammerhead.org/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
