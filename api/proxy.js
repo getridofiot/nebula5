606463e09799f7e1999368094bdd737263b2059d
getridofiot <getridofthem@protonmail.com>
2024-11-03 15:29:30 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://phantom.lol/";//your website url
+  let target = "https://anura.pro/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
